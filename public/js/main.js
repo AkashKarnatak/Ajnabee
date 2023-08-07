019ec58c58c8bcc2a34921b4190bced7bf919dd0
AkashKarnatak <akashkarnatak.ak47@gmail.com>
2023-08-07 13:47:53 +0530

    Added multiple stun servers

--- public/js/main.js
+++ public/js/main.js
@@ -46,7 +46,15 @@ WebSocket.prototype.propagate = function (channel, data) {
 async function initializeConnection() {
   const iceConfig = {
     iceServers: [
-      { urls: 'stun:stun.l.google.com:19302' },
+      {
+        urls: [
+          'stun:stun.l.google.com:19302',
+          'stun:stun1.l.google.com:19302',
+          'stun:stun2.l.google.com:19302',
+          'stun:stun3.l.google.com:19302',
+          'stun:stun4.l.google.com:19302',
+        ]
+      },
     ],
   }
 
