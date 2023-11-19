4f3ffe1d6766300c7dab22626b4daf424bc29ed1
Dhruv Badaya <dhruvbadaya@gmail.com>
2023-11-19 13:03:53 +0530

    Added Favicon Files and Code of Conduct
    [x] Added Favicon Files for Desktop
    [x] Added Favicon Files for iOS
    [x] Added Favicon Files for Android
    [x] Added Favicon Site Manifest
    [x] Added Convenant Code of Conduct

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { WebSocket, WebSocketServer } from 'ws'
 import db from './db/sqlite.js'
 
-const SERVER_PORT = process.env.SERVER_PORT
+const SERVER_PORT = 8000
 
 if (!SERVER_PORT) {
   throw new Error('Forgot to initialze some variables')
